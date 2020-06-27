e48dd6d878f21015747d5682d9e61d19d001fb7e
k_sakata <k_sakata@pal-style.co.jp>
2020-06-28 00:55:26 +0900

    settings.py LANGUAGE_CODE = jaに変更 及び setting.json修正

--- djangoapi/settings.py
+++ djangoapi/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'api.apps.ApiConfig',
+    'api.apps.ApiConfig'
 ]
 
 MIDDLEWARE = [
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
 TIME_ZONE = 'Asia/Tokyo'
 
